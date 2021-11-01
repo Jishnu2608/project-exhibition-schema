dc78bc315fa527b456708b785eb0cfd238d8ce48
Jishnu2608 <jishnudeepborah@gmail.com>
2021-11-02 01:53:45 +0530

    hooks and helpers added, scss initiated

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,5 @@
-import firebase from "firebase/app";
-import 'firebase/firestore';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
 
 const firebaseConfig = firebase.initializeApp({
     apiKey: "AIzaSyBbil0uLVfWCZDKxTxN1roHjqSzG_4299I",
